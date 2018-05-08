70cc2cf6521f8171ef34b62d455316d3d8f48084
Galen Gold <galen@galengold.me>
2018-05-08 00:39:09 -0400

    first tests

--- qrmbot.py
+++ qrmbot.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
 import discord
+import asyncio
+import json
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+with open('secrets.json') as secrets_file:
+    secrets = json.load(secrets_file)
+
+client.run(secrets['token'])
 
